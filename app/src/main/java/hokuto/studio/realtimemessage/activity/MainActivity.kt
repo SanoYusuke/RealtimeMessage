daa1b87a4364ddc52818a06b8315e5d4f0131aeb
Sano Yusuke <sano.yusuke.teco@gmail.com>
2018-05-14 12:14:05 +0900

    [Add] Manifest.xmlにInternet接続の許可を追加

--- app/src/main/java/hokuto/studio/realtimemessage/activity/MainActivity.kt
+++ app/src/main/java/hokuto/studio/realtimemessage/activity/MainActivity.kt
@@ -2,14 +2,12 @@ package hokuto.studio.realtimemessage.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.database.*
 import hokuto.studio.realtimemessage.R
-
 
 class MainActivity : AppCompatActivity() {
 
